5fa5c0c8b8024ffe605f7720e5fa423240f7ecad
ruizqa <89569188+ruizqa@users.noreply.github.com>
2021-09-19 22:45:26 -0600

    adding styling changes

--- server.py
+++ server.py
@@ -7,21 +7,33 @@ app.secret_key = 'secret'
 
 @app.route('/')         
 def visits():
+    
     if 'visits' in session:
         session['visits'] = session['visits'] + 1  # reading and updating session data
     else:
         session['visits'] = 1
+
+    if 'counter' not in session:
+        session['counter'] =0
+    
     return render_template("index.html")
 
 @app.route('/destroy_session')         
 def destroy():
     session.pop('visits', None)
+    session.pop('counter',None)
     return redirect ("/")
 
 @app.route('/add2')         
 def add2():
     session['visits'] = session['visits'] + 1
     return redirect ("/")
+
+@app.route('/checkout', methods=['POST'])  
+def checkout():
+    session['counter'] += int(request.form['number']) -1
+    return redirect ("/")
+ 
 
 if __name__=="__main__":   
     app.run(debug=True)    
